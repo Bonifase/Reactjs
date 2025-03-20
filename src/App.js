f2b7e1e3821db2c1de8bd8d656d7f795d78fd634
Bonifase <orwabonifase@gmail.com>
2025-03-20 16:58:55 +0300

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
+function Square({value, onSquareClick}) {
 
   return (
-    <>
-      <Button variant="contained" color="success" onClick={handleClick}>
-        Click me
-      </Button>
-      <Button variant="contained" color="success">
-        {count}
-      </Button>
-      <ul>{listItems}</ul>
-    </>
+    <Button variant="contained" color="info" size="medium" className="square" onClick={onSquareClick}>
+      {value}
+    </Button>
+  )
+}
 
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    const nextSquares = squares.slice();
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ '& button': { m: 0 } }}>
+        <div className="board-row">
+          {[0, 1, 2].map(val => <Square value={squares[val]} onSquareClick={() => handleClick(val)}/>)}
+        </div>
+        <div className="board-row">
+          {[3, 4, 5].map(val => <Square value={squares[val]} onSquareClick={() => handleClick(val)}/>)}
+        </div>
+        <div className="board-row">
+          {[6, 7, 8].map(val => <Square value={squares[val]} onSquareClick={() => handleClick(val)}/>)}
+        </div>
+      </Box>
+    </Container>
   );
 }
 
